2874b47b0ad07d339238aea352a553c4fbd4a058
GeorgeKuncheria <chempumthara.g@northeastern.edu>
2024-08-15 19:25:44 -0400

    fixed middleware 1.0

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -57,6 +57,9 @@ export async function POST(req: Request) {
   const { id } = evt.data;
   const eventType = evt.type;
 
+    console.log('Received event:', evt.type);
+    console.log('User data:', evt.data);
+
   // CREATE
   if (eventType === "user.created") {
     const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
